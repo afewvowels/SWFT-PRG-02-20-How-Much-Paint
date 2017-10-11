90699ee67e05f0f7a9e7dc4bf4eeb444822289ad
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-11 12:28:31 -0400

    Initial Commit

---
+++ SWFT-PRG-02-20-How-Much-Paint/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SWFT-PRG-02-20-How-Much-Paint
+//
+//  Created by Keith Smith on 10/11/17.
+//  Copyright © 2017 Keith Smith. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
